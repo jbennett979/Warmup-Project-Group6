d820ac15465aa27e5c9ef85c9876d092f9b55527
GavinMonteiro <gtmontei@uvm.edu>
2026-01-31 13:46:27 -0500

    Testing out access to firestore.

--- admin.py
+++ admin.py
@@ -1,3 +1,12 @@
-
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import credentials, firestore
+
+# Accessing service key to allow changes to be made to firestore project
+# DO NOT ADD SERVICEACCOUNT TO REPO
+cred = credentials.Certificate("serviceAccount.json")
+firebase_admin.initialize_app(cred)
+
+# Variable to access database from firestore
+db = firestore.client()
+
+# TODO: Send women_in_software_engineering_stats.json to database
