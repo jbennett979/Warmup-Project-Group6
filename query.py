254b3b7e8597c357f1bb06d56b5564c82b3a0edf
jbennett979 <jabennet@uvm.edu>
2026-02-03 11:04:56 -0500

    Added todos in query.py

--- query.py
+++ query.py
@@ -1,1 +1,5 @@
+# TODO: a function that allows the user to query then calls the parser function
 
+# TODO: a parser function
+
+# TODO: a function that takes in the parsed tokens, and interacts with the data
