bbb578704593f830706835f1002c20638b905887
kimch <kimchung2502@gmail.com>
2026-02-06 10:31:06 -0500

    test_queries.py: can now pass list to .where() as arg

--- test_queries.py
+++ test_queries.py
@@ -3,7 +3,9 @@ import authentication
 db = authentication.authenticate()
 collection_ref = db.collection("Women in Software Engineering")
 
-query = collection_ref.where('company', '==', 'GitHub').stream()
+parsed_query = ['company', '==', 'GitHub']
+
+query = collection_ref.where(*parsed_query).stream()
 
 for doc in query:
     print(f'{doc.id} => {doc.to_dict()}')
